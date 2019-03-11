5160c5cddcb5d3867608a818c8ad917d189ca8c1
Sushi21 <yannick.dufils@gmail.com>
2019-03-11 13:50:41 -0300

    some reformating

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
-const { ApolloServer, AuthenticationError } = require('apollo-server');
-const moogoose = require('mongoose');
-const fs = require('fs');
-const path = require('path');
-const jwt = require('jsonwebtoken');
+const { ApolloServer, AuthenticationError } = require('apollo-server')
+const moogoose = require('mongoose')
+const fs = require('fs')
+const path = require('path')
+const jwt = require('jsonwebtoken')
 
 // Import typeDefs and resolvers
-const filePath = path.join(__dirname, 'typeDefs.gql');
-const typeDefs = fs.readFileSync(filePath, 'utf-8');
-const resolvers = require('./resolvers');
+const filePath = path.join(__dirname, 'typeDefs.gql')
+const typeDefs = fs.readFileSync(filePath, 'utf-8')
+const resolvers = require('./resolvers')
 
 //Import Enverinment Variables and Mongoose Models
-require('dotenv').config({ path: 'variables.env' });
-const User = require('./models/User');
-const Post = require('./models/Post');
+require('dotenv').config({ path: 'variables.env' })
+const User = require('./models/User')
+const Post = require('./models/Post')
 
 // Connect to Mlab Database
 moogoose
-  .connect(
-    process.env.MONGO_URI,
-    { useNewUrlParser: true }
-  )
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => console.log('DB Connected'))
-  .catch(err => console.error(err));
+  .catch(err => console.error(err))
 
 // Create Apollo/GraphQl Server using typeDefs, resolvers, and context object
 const server = new ApolloServer({
@@ -32,26 +29,26 @@ const server = new ApolloServer({
     message: error.message.replace('Context creation failed:', '')
   }),
   context: async ({ req }) => {
-    const token = req.headers['authorization'];
+    const token = req.headers['authorization']
 
-    return { User, Post, currentUser: await getUser(token) };
+    return { User, Post, currentUser: await getUser(token) }
   }
-});
+})
 
 // Verify JWT Token passed from client
 
 const getUser = async token => {
   if (token) {
     try {
-      return await jwt.verify(token, process.env.SECRET);
+      return await jwt.verify(token, process.env.SECRET)
     } catch (err) {
       throw new AuthenticationError(
         'Your session has ended. Please sign in again.'
-      );
+      )
     }
   }
-};
+}
 
-server.listen().then(({ url }) => {
-  console.log(`Server listening on ${url}`);
-});
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`Server listening on ${url}`)
+})
