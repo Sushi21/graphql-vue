ebb836d9c99dc03434f3ab77205f2937fac3e774
Sushi21 <yannick.dufils@gmail.com>
2019-03-11 12:03:18 -0300

    re run query one creation

--- client/src/store.js
+++ client/src/store.js
@@ -12,7 +12,8 @@ import {
   SEARCH_POSTS,
   GET_USER_POSTS,
   UPDATE_USER_POST,
-  DELETE_USER_POST
+  DELETE_USER_POST,
+  INFINITE_SCROLL_POSTS
 } from './queries'
 
 Vue.use(Vuex)
@@ -136,7 +137,17 @@ export default new Vuex.Store({
               _id: -1,
               ...payload
             }
-          }
+          },
+          // rerun scecified queries after perfmiring the mutation
+          refetchQueries: [
+            {
+              query: INFINITE_SCROLL_POSTS,
+              variables: {
+                pageNum: 1,
+                pageSize: 2
+              }
+            }
+          ]
         })
         .then(({ data }) => {
           console.log(data.addPost)
